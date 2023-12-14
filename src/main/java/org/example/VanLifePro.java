44711b532987596c1a9f52c2733062419aa2c481
MaisonEP <erl.maison@hotmail.co.uk>
2023-12-14 15:23:01 +0000

    adding git ignore

--- src/main/java/org/example/VanLifePro.java
+++ src/main/java/org/example/VanLifePro.java
@@ -3,11 +3,17 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Stack;
+
 @SpringBootApplication
 public class VanLifePro {
 
     public static void main(String[] args) {
         SpringApplication.run(VanLifePro.class, args);
     }
+
+   Stack  myStack = new Stack<>();
+
+
 }
 
